c211da3dd620732c8d443a4339773f571aeef460
Harsh Singh <harsh_singh@srmap.edu.in>
2025-03-25 11:14:14 +0530

    File I/O to get file properties

---
+++ Java_io/Fileio.java
@@ -0,0 +1,26 @@
+import java.io.File;
+
+class Fileio {
+    static void p(String s) {
+        System.out.println(s);
+    }
+
+    public static void main(String args[]) {
+        // Specify the file path (Change this to an existing file path)
+        File f1 = new File("Introduction.txt");
+
+        if (f1.exists()) { // Check if the file exists
+            p("File Name: " + f1.getName());
+            p("Absolute Path: " + f1.getAbsolutePath());
+            p("Parent Directory: " + f1.getParent());
+            p("File Size: " + f1.length() + " bytes");
+            p("Readable: " + f1.canRead());
+            p("Writable: " + f1.canWrite());
+            p("Executable: " + f1.canExecute());
+            p("Is a File: " + f1.isFile());
+            p("Is a Directory: " + f1.isDirectory());
+        } else {
+            p("File does not exist.");
+        }
+    }
+}
